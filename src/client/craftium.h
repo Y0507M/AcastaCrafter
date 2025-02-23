f4cea70912b7a7e872a8f791927f390eae792391
Yash Pathak <yashpathak1446@gmail.com>
2025-02-23 13:39:31 -0800

    Added more mods, locale, and other dependencies

---
+++ src/client/craftium.h
@@ -0,0 +1,220 @@
+#pragma once
+
+#include "keys.h"
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+#include "../settings.h"
+
+
+inline char actions[27];
+
+/*
+
+  Frameskip
+  ~~~~~~~~~
+
+*/
+inline int frameskip_count = 0;
+// The value of this variable is set in the Client::startPyConn
+// method according to the frameskip setting
+inline int frameskip = 0;
+
+/*
+
+  "Virtual" keyboard input handling
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
+inline bool virtual_key_presses[KeyType::INTERNAL_ENUM_COUNT];
+inline int virtual_mouse_x = 0;
+inline int virtual_mouse_y = 0;
+
+
+/*
+
+  Synchronization between minetest's server and client
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
+
+inline char *srv_sem_name_A;
+inline char *srv_sem_name_B;
+inline sem_t *srv_sem_A = nullptr;
+inline sem_t *srv_sem_B = nullptr;
+
+inline char *cli_sem_name_A;
+inline char *cli_sem_name_B;
+inline sem_t *cli_sem_A = nullptr;
+inline sem_t *cli_sem_B = nullptr;
+
+inline int syncServerInit()
+{
+    // Create the name of the semaphore based on the port number that commicates MT server and client
+    srv_sem_name_A = (char*)malloc(20 * sizeof(char));
+    srv_sem_name_B = (char*)malloc(20 * sizeof(char));
+    sprintf(srv_sem_name_A, "/crftA%d", g_settings->getU16("port"));
+    sprintf(srv_sem_name_B, "/crftB%d", g_settings->getU16("port"));
+
+    /*
+    printf("Creating semaphore with name '%s'\n", srv_sem_name_A);
+    printf("Creating semaphore with name '%s'\n", srv_sem_name_B);
+    */
+
+    // Create the sempaphores with an initial value of 1
+    if ((srv_sem_A = sem_open(srv_sem_name_A, O_CREAT, 0644, 0)) == SEM_FAILED) {
+        perror("[SyncServer] Failed to create semaphore A");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((srv_sem_B = sem_open(srv_sem_name_B, O_CREAT, 0644, 0)) == SEM_FAILED) {
+        perror("[SyncServer] Failed to create semaphore B");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
+inline int syncClientInit() {
+    cli_sem_name_A = (char*)malloc(20 * sizeof(char));
+    cli_sem_name_B = (char*)malloc(20 * sizeof(char));
+    sprintf(cli_sem_name_A, "/crftA%d", g_settings->getU16("port"));
+    sprintf(cli_sem_name_B, "/crftB%d", g_settings->getU16("port"));
+
+    /*
+    printf("Creating semaphore with name '%s'\n", cli_sem_name_A);
+    printf("Creating semaphore with name '%s'\n", cli_sem_name_B);
+    */
+
+    if ((cli_sem_A = sem_open(cli_sem_name_A, 0)) == SEM_FAILED) {
+        perror("[SyncClient] Failed  to open semaphore A");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((cli_sem_B = sem_open(cli_sem_name_B, 0)) == SEM_FAILED) {
+        perror("[SyncClient] Failed  to open semaphore B");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
+inline void syncServerStep() {
+    if (!g_settings->getBool("sync_env_mode")) {
+        return;
+    }
+
+    // printf("<= SRV start\n");
+
+    if (srv_sem_A == nullptr)
+        syncServerInit();
+
+
+    if (sem_post(srv_sem_B) < 0) {
+        perror("[SyncServerStep] Error posting sem B");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sem_wait(srv_sem_A) < 0) {
+        perror("[SyncServerStep] Error waiting sem A");
+        exit(EXIT_FAILURE);
+    }
+
+    // printf("=> SRV end\n");
+}
+
+inline void syncClientStep() {
+    if (!g_settings->getBool("sync_env_mode")) {
+        return;
+    }
+    // printf("> CLI start\n");
+
+    if (cli_sem_A == nullptr)
+      syncClientInit();
+
+    if (sem_post(cli_sem_A) < 0) {
+        perror("[SyncServerStep] Error posting sem A");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sem_wait(cli_sem_B) < 0) {
+        perror("[SyncServerStep] Error waiting sem B");
+        exit(EXIT_FAILURE);
+    }
+
+    // printf("< CLI end\n");
+}
+
+/*
+
+  Reward and termination signal system
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
+inline double g_reward = 0.0; /* Global variable of the reward value */
+
+/* Values used by `lua_set_reward_once`: */
+inline bool g_reward_reset = false;  /* Whether to reset the reward value after one iteration */
+inline double g_reward_reset_value = 0.0; /* The value to reset the reward to */
+
+inline bool g_termination = false; /* Global variable with the termination flag */
+inline bool g_soft_reset = false; /* Global variable with the termination flag */
+
+extern "C" {
+#include <lualib.h>
+}
+
+/* Implementation of the Lua functions to get/set the global reward value */
+inline static int lua_set_reward(lua_State *L) {
+    double d = lua_tonumber(L, 1);  /* get argument */
+    g_reward = d;
+    return 0; /* number of results */
+}
+
+inline static int lua_set_reward_once(lua_State *L) {
+    /* get the two arguments */
+    double val = lua_tonumber(L, 1);
+    double reset_val = lua_tonumber(L, 2);
+
+    g_reward = val;
+    g_reward_reset_value = reset_val;
+    g_reward_reset = true;
+
+    return 0; /* number of results */
+}
+
+inline static int lua_get_reward(lua_State *L) {
+    lua_pushnumber(L, g_reward);
+    return 1; /* number of results */
+}
+
+/* Implementation of the Lua functions to get/set the global termination flag */
+inline static int lua_set_termination(lua_State *L) {
+    g_termination = true;
+    return 0; /* number of results */
+}
+
+inline static int lua_reset_termination(lua_State *L) {
+    g_termination = false;
+    g_soft_reset = false;
+    return 0; /* number of results */
+}
+
+inline static int lua_get_termination(lua_State *L) {
+    lua_pushnumber(L, (int)g_termination);
+    return 1; /* number of results */
+}
+
+inline static int lua_get_soft_reset(lua_State *L) {
+    lua_pushnumber(L, (int)g_soft_reset);
+    return 1; /* number of results */
+}
---
+++ src/script/scripting_client.cpp
@@ -0,0 +1,114 @@
+/*
+Minetest
+Copyright (C) 2013 celeron55, Perttu Ahola <celeron55@gmail.com>
+Copyright (C) 2017 nerzhul, Loic Blot <loic.blot@unix-experience.fr>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "scripting_client.h"
+#include "client/client.h"
+#include "cpp_api/s_internal.h"
+#include "lua_api/l_client.h"
+#include "lua_api/l_env.h"
+#include "lua_api/l_item.h"
+#include "lua_api/l_itemstackmeta.h"
+#include "lua_api/l_minimap.h"
+#include "lua_api/l_modchannels.h"
+#include "lua_api/l_particles_local.h"
+#include "lua_api/l_storage.h"
+#include "lua_api/l_util.h"
+#include "lua_api/l_item.h"
+#include "lua_api/l_nodemeta.h"
+#include "lua_api/l_localplayer.h"
+#include "lua_api/l_camera.h"
+#include "lua_api/l_settings.h"
+#include "lua_api/l_client_sound.h"
+
+#include "../client/craftium.h"
+
+ClientScripting::ClientScripting(Client *client):
+	ScriptApiBase(ScriptingType::Client)
+{
+	setGameDef(client);
+
+	SCRIPTAPI_PRECHECKHEADER
+
+	// Security is mandatory client side
+	initializeSecurityClient();
+
+	lua_getglobal(L, "core");
+	int top = lua_gettop(L);
+
+        /* Functions to get/set the global reward and termination values */
+        lua_register(L, "set_reward", lua_set_reward);
+        lua_register(L, "set_reward_once", lua_set_reward_once);
+        lua_register(L, "get_reward", lua_get_reward);
+        lua_register(L, "set_termination", lua_set_termination);
+        lua_register(L, "reset_termination", lua_reset_termination);
+        lua_register(L, "get_termination", lua_get_termination);
+        lua_register(L, "get_soft_reset", lua_get_soft_reset);
+
+	lua_newtable(L);
+	lua_setfield(L, -2, "ui");
+
+	InitializeModApi(L, top);
+	lua_pop(L, 1);
+
+	// Push builtin initialization type
+	lua_pushstring(L, "client");
+	lua_setglobal(L, "INIT");
+
+	infostream << "SCRIPTAPI: Initialized client game modules" << std::endl;
+}
+
+void ClientScripting::InitializeModApi(lua_State *L, int top)
+{
+	LuaItemStack::Register(L);
+	ItemStackMetaRef::Register(L);
+	LuaRaycast::Register(L);
+	StorageRef::Register(L);
+	LuaMinimap::Register(L);
+	NodeMetaRef::RegisterClient(L);
+	LuaLocalPlayer::Register(L);
+	LuaCamera::Register(L);
+	ModChannelRef::Register(L);
+	LuaSettings::Register(L);
+	ClientSoundHandle::Register(L);
+
+	ModApiUtil::InitializeClient(L, top);
+	ModApiClient::Initialize(L, top);
+	ModApiItem::InitializeClient(L, top);
+	ModApiStorage::Initialize(L, top);
+	ModApiEnv::InitializeClient(L, top);
+	ModApiChannels::Initialize(L, top);
+	ModApiParticlesLocal::Initialize(L, top);
+	ModApiClientSound::Initialize(L, top);
+}
+
+void ClientScripting::on_client_ready(LocalPlayer *localplayer)
+{
+	LuaLocalPlayer::create(getStack(), localplayer);
+}
+
+void ClientScripting::on_camera_ready(Camera *camera)
+{
+	LuaCamera::create(getStack(), camera);
+}
+
+void ClientScripting::on_minimap_ready(Minimap *minimap)
+{
+	LuaMinimap::create(getStack(), minimap);
+}
